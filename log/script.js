2c30321d183d17f5548b31e5df5d03c8ba5cd4b9
Wspyder <148482366+Wspyder@users.noreply.github.com>
2024-03-23 04:09:58 +0530

    Add files via upload

---
+++ log/script.js
@@ -0,0 +1,41 @@
+document.querySelector('.signup-link').addEventListener('click', () => {
+    document.querySelector('.signup').style.visibility = "visible";
+    document.querySelector('.login').style.visibility = "hidden";
+})
+
+document.querySelector('.login-link').addEventListener('click', () => {
+    document.querySelector('.signup').style.visibility = "hidden";    
+    document.querySelector('.login').style.visibility = "visible";
+})
+
+
+document.querySelector('.eye-icon').addEventListener('click', () => {
+    alert("not enough budget :(")
+})
+
+document.querySelector('.media-options').addEventListener('click', () => (
+    alert('not enough budget :('))
+    )
+    
+// document.querySelector(".button-field").addEventListener('click', () => {
+//         alert('not enough budget :(')
+// })
+
+document.querySelector(".a1").addEventListener('click', () => {
+        alert('not enough budget :(')
+})
+
+document.querySelector(".a2").addEventListener('click', () => {
+    alert('not enough budget :(')
+})
+
+document.querySelector(".a3").addEventListener('click', () => {
+    alert('not enough budget :(')
+})
+
+document.querySelector(".a5").addEventListener('click', () => {
+    alert('not enough budget :(')
+})
+document.querySelector(".a6").addEventListener('click', () => {
+    alert('not enough budget :(')
+})
